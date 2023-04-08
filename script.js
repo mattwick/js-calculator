06d5da6bcf5ed25e11400a1afa50c50089c5524c
mattwick <mattwick@gmail.com>
2023-04-08 15:39:44 -0500

    added new relative difference feature

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const display = document.querySelector('.display');
 const historyList = document.querySelector('.history-list');
+const inputField = document.getElementById('input-field');
+const historyContent = document.getElementById('history-content');
 let resultCalculated = false;
 window.addEventListener('keydown', handleKeyDown);
 
@@ -40,36 +42,70 @@ function clearDisplay() {
 }
 
 function calculate() {
+    const expression = inputField.value.trim();
+    if (expression === "") return;
+
     try {
-        const inputValue = display.value;
-        const result = eval(inputValue);
-        display.value = result;
-        addToHistory(inputValue, result);
-        resultCalculated = true;
+        const preparedExpression = expression.replace(/×/g, "*").replace(/÷/g, "/");
+        const result = math.evaluate(preparedExpression); // Directly use math.evaluate()
+        inputField.value = result.toFixed(3).replace(/\.?0+$/, "");
+        addToHistory(expression, result);
     } catch (error) {
-        display.value = 'Error';
+        console.error("Error:", error);
+        inputField.value = "Error";
     }
 }
 
-function addToHistory(input, result) {
+function addToHistory(expression, result) {
     const historyItem = document.createElement('div');
     historyItem.classList.add('history-item');
 
-    const formattedInput = formatInput(input);
-    const shortResult = formatResult(result);
-    const inputDisplay = document.createElement('span');
-    inputDisplay.textContent = `${formattedInput} = ${shortResult}`;
-    historyItem.appendChild(inputDisplay);
+    // Add copy button
+    const copyButton = document.createElement('button');
+    copyButton.classList.add('copy-btn');
+    copyButton.textContent = '📋';
+    copyButton.onclick = (event) => { // Add event parameter
+        event.stopPropagation(); // Prevent triggering other click events on parent elements
+        copyToClipboard(result); // Update this line
+      };
+    historyItem.appendChild(copyButton);
 
-    const addButton = document.createElement('button');
-    addButton.textContent = '+';
-    addButton.addEventListener('click', () => {
-        appendNumber(result);
-    });
+
+    const historyExpression = document.createElement('span');
+    historyExpression.classList.add('history-expression');
+    historyExpression.textContent = expression;
+    historyItem.appendChild(historyExpression);
+
+    const historyResult = document.createElement('span');
+    historyResult.classList.add('history-result');
+    historyResult.textContent = formatResult(result);
+    historyItem.appendChild(historyResult);
+
+    // Add add button
+    const addButton = document.createElement("button");
+    addButton.textContent = "+";
+    addButton.classList.add("history-add");
+    addButton.onclick = () => addToCalculator(result);
     historyItem.appendChild(addButton);
-
-    historyList.prepend(historyItem);
+    historyContent.insertBefore(historyItem, historyContent.firstChild);
 }
+
+function copyToClipboard(text) {
+  const textarea = document.createElement('textarea');
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+}
+
+function addToCalculator(value) {
+  if (inputField.value === "Error") {
+    inputField.value = "";
+  }
+  inputField.value += value;
+}
+
 
 function formatInput(input) {
     const inputRegex = /(\d+\.\d{1,2})\d*/g;
@@ -82,4 +118,16 @@ function formatResult(result) {
         return `${formattedResult}...`;
     }
     return formattedResult;
+}
+
+function calculateRelativeDifference() {
+    const originalValue = parseFloat(document.getElementById("original-value").value);
+    const newValue = parseFloat(document.getElementById("new-value").value);
+    if (!isNaN(originalValue) && !isNaN(newValue) && originalValue !== 0) {
+        const relativeDifference = ((newValue - originalValue) / originalValue) * 100;
+        const resultElement = document.getElementById("relative-difference-result");
+
+        resultElement.textContent = `${relativeDifference.toFixed(2)}%`;
+        resultElement.style.color = relativeDifference >= 0 ? 'green' : 'red';
+    }
 }
